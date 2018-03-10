ecd622a4340ec104aeef2dc0defc1287f86c5924
wsjhk <2621570425@qq.com>
2018-03-10 14:52:15 +0800

    Update refresh_script.py

--- refresh_script.py
+++ refresh_script.py
@@ -7,12 +7,13 @@ from datetime import datetime
 
 # db.create_all()
 
+#获取阿里云接口认证的token
 token = huanju_aliyun_cdn_api.GetApiToken()
 
-def refresh():
-    id_list,url_list = [],[]
+#将数据库中记录状态为1的等待刷新的资源提交到阿里云上刷新，提交之后修改状态为2刷新中
+def refresh(type):
     try:
-        info = cdn_info_new.query.filter_by(status="1").all()
+        info = cdn_info_new.query.filter_by(status="1").filter_by(type=type).all()
     except Exception:
         print "DB Error."
         return None
@@ -20,26 +21,12 @@ def refresh():
         db.session.close()
     if info:
         for i in info:
-            id_list.append(i.id)
-            url_list.append(i.url_dir)
-        item_id = huanju_aliyun_cdn_api.RefreshCdn(url_list, "flush", token)
-
-        if "," in item_id:
-            url_id = item_id.split(",")[0]
-            dir_id = item_id.split(",")[1]
-        else:
-            url_id,dir_id = item_id,item_id
-
-        for j in range(0, len(id_list)):
+            url = [i.resources]
+            item_id = huanju_aliyun_cdn_api.RefreshCdn(url, "flush", token)
             try:
-                if url_list[j][len(url_list[j]) - 1] == "/":
-                    update = cdn_info_new.query.filter_by(id=id_list[j]).first()
-                    update.status = "2"
-                    update.RefreshTaskId = dir_id
-                else:
-                    update = cdn_info_new.query.filter_by(id=id_list[j]).first()
-                    update.status = "2"
-                    update.RefreshTaskId = url_id
+                update = cdn_info_new.query.filter_by(id=i.id).first()
+                update.status = "2"
+                update.RefreshTaskId = item_id
                 db.session.commit()
             except Exception:
                 print "DB Error."
@@ -49,8 +36,8 @@ def refresh():
     else:
         pass
 
+#将数据库中状态为2的刷新中的资源的item_id取出用于查询在阿里云上的刷新进度，查询之后更新进度的，如果进度100%则更新状态为3已刷新。
 def select():
-    id_list,item_id_list = [],[]
     try:
         info1 = cdn_info_new.query.filter_by(status="2").all()
     except Exception:
@@ -60,30 +47,25 @@ def select():
         db.session.close()
     if info1:
         for i in info1:
-            # id_list.append(i.id)
-            item_id_list.append(i.RefreshTaskId)
-        item_id_list = list(set(item_id_list))
-        for j in item_id_list:
-            res = huanju_aliyun_cdn_api.QueryCdn(str(j), token)
-            for k,v in res.items():
-                try:
-                    update = cdn_info_new.query.filter_by(url_dir=k).filter_by(RefreshTaskId=j).all()
-                    for one in update:
-                        one.Process = v['rate']
-                        if v['rate'] == "100%":
-                            one.status = "3"
-                        else:
-                            one.status = "2"
-                        db.session.commit()
-                except Exception:
-                    print "DB Error."
-                    return None
-                finally:
-                    db.session.close()
+            item_id = str(i.RefreshTaskId)
+            res = huanju_aliyun_cdn_api.QueryCdn(item_id, token)
+            try:
+                update = cdn_info_new.query.filter_by(id=i.id).first()
+                update.Process = res[i.resources]['rate']
+                if res[i.resources]['rate'] == "100%":
+                    update.status = "3"
+                else:
+                    update.status = "2"
+                db.session.commit()
+            except Exception:
+                print "DB Error."
+                return None
+            finally:
+                db.session.close()
     else:
         pass
 
-
+#将数据库中状态为2的刷新中的资源，核对提交的时间和现在的时间差，如果超过半小时，则认为刷新失败，修改状态为4刷新失败。
 def shibai():
     try:
         info2 = cdn_info_new.query.filter_by(status="2").all()
@@ -104,18 +86,18 @@ def shibai():
     finally:
         db.session.close()
 
-
 if __name__ == '__main__':
-    print("[" + datetime.now() + "]: task start.")
-    threads = []
-    t1 = threading.Thread(target=refresh)
-    t2 = threading.Thread(target=select)
-    t3 = threading.Thread(target=shibai)
-    threads.append(t1)
-    threads.append(t2)
-    threads.append(t3)
-    for t in threads:
-        t.setDaemon(True)
-        t.start()
-        time.sleep(15)
-    print("[" + datetime.now() + "]: task end.")
+    while 1:
+        threads = []
+        t1 = threading.Thread(target=refresh, args=("url",))
+        t2 = threading.Thread(target=refresh, args=("dir",))
+        t3 = threading.Thread(target=select)
+        t4 = threading.Thread(target=shibai)
+        threads.append(t1)
+        threads.append(t2)
+        threads.append(t3)
+        threads.append(t4)
+        for t in threads:
+            t.setDaemon(True)
+            t.start()
+        time.sleep(5)
