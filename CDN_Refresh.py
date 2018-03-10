ac298db5e42a1ac8ee49c5bb5009f74cc94cc8da
wsjhk <2621570425@qq.com>
2018-03-10 14:51:22 +0800

    Update CDN_Refresh.py

--- CDN_Refresh.py
+++ CDN_Refresh.py
@@ -24,29 +24,23 @@ def login_required(func):
 
 #刷新cdn时定义的用于保存接收页面传递的cdn信息和执行刷新操作的类
 class refresh(object):
-    def __init__(self,domain,dir):
+    def __init__(self, domain, dir):
         self.domains = list(set(domain))        #将接收的地址进行去重处理
         self.dirs = list(set(dir))
 
-    def refreshop(self):
-        url_list,dir_list = [],[]
-        for domain in self.domains:
-            if domain:          #判断是否是空行的情况，是的话什么也不做
-                url_list.append(domain)
+    def commit(self, resources, type):
+        resource_list = []
+        for resource in resources:
+            if resource:
+                resource_list.append(resource)
             else:
                 continue
-        for dir in self.dirs:
-            if dir:
-                url_list.append(dir)
-                dir_list.append(dir)
-            else:
-                continue
-        if len(url_list) <= 1 and url_list[0] == '':
-            data = {'status': "kong"}
+        if len(resource_list) <= 1 and resource_list[0] == 'none':
+            status = {'status': "kong"}
         else:
-            for i in url_list:
+            for i in resource_list:
                 try:
-                    info = cdn_info_new(0,i,"0%",session.get('user_username'),"1")
+                    info = cdn_info_new(0,i,type,"0%",session.get('user_username'),"1")
                     db.session.add(info)
                     db.session.commit()
                 except Exception:
@@ -54,6 +48,15 @@ class refresh(object):
                     return None
                 finally:
                     db.session.close()
+            status = {'status': "OK"}
+        return status
+
+    def refreshop(self):
+        url = self.commit(self.domains, "url")
+        dir = self.commit(self.dirs, "dir")
+        if url['status'] == "kong" and dir['status'] == "kong":
+            data = {'status': "kong"}
+        else:
             data = {'status': "OK"}
         return data
 
@@ -70,14 +73,14 @@ def run_command(cmd):
     return result
 
 #登录视图函数
-@app.route('/', methods=['GET','POST'])
-@app.route('/login', methods=['GET','POST'])
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
         try:
-            user = auth_user.query.filter_by(username = username,password = password).first()
+            user = auth_user.query.filter_by(username=username, password=password).first()
         except Exception:
             print "DB Error."
             return None
@@ -92,26 +95,26 @@ def login():
         return render_template('login.html')
 
 #YY登录进行验证，成功后，回调 yy_oauth函数进行创用户
-@app.route('/yy_cdnlogin', methods=['GET','POST'])
+@app.route('/yy_cdnlogin', methods=['GET', 'POST'])
 def yy_cdnlogin():
-    proc = run_command('php ./yy_oauth/login.php')
+    proc = run_command('php .\yy_oauth\login.php')
     return make_response(redirect(proc['out']))
 
 #YY登录回调的url,首次登录会创建用户
-@app.route('/yy_cdnoauth', methods=['GET','POST'])
+@app.route('/yy_cdnoauth', methods=['GET', 'POST'])
 def yy_cdnoauth():
     oauth_token = request.args.get('oauth_token')
     oauth_verifier = request.args.get('oauth_verifier')
-    proc = run_command(str("php ./yy_oauth/login.php do=yy_oauth oauth_token="+oauth_token+" oauth_verifier="+oauth_verifier))
+    proc = run_command(str("php .\yy_oauth\login.php do=yy_oauth oauth_token="+oauth_token+" oauth_verifier="+oauth_verifier))
     script_response = proc['out']
     yy_user = script_response.split('-')
     username = yy_user[0]
     yy_email = username[3:]+"@yy.com"
     password = yy_user[0]+"@yy.com"
     try:
-        user_info = auth_user.query.filter_by(username = username).first()
-        if user_info is None and username.startswith("dw_"):
-            add_user = auth_user(username,yy_email,password)
+        user_info = auth_user.query.filter_by(username=username).first()
+        if user_info is None:
+            add_user = auth_user(username, yy_email, password)
             db.session.add(add_user)
             db.session.commit()
     except Exception:
@@ -123,22 +126,26 @@ def yy_cdnoauth():
     return redirect(url_for('dashboard'))
 
 #YY登录拒绝回调的url
-@app.route('/yy_denyCallback', methods=['GET','POST'])
+@app.route('/yy_denyCallback', methods=['GET', 'POST'])
 def yy_denyCallback():
     pass
 
 #主功能刷新视图函数
-@app.route('/dashboard', methods=['GET','POST'])
+@app.route('/dashboard', methods=['GET', 'POST'])
 @login_required
 def dashboard():
     if request.method == 'POST':
-        domains = request.form.get('domain').split('\n')
-        dirs = request.form.get('dir').split('\n')
-        curr = refresh(domains,dirs)
+        domains = request.form.get('domain').strip().split('\n')
+        dirs = request.form.get('dir').strip().split('\n')
+        if dirs[0] == "":
+            dirs[0] = "none"
+        if domains[0] == "":
+            domains[0] = "none"
+        curr = refresh(domains, dirs)
         data = curr.refreshop()
         return jsonify(data)
     else:
-        return render_template('dashboard.html',user = session['user_username'])
+        return render_template('dashboard.html', user=session['user_username'])
 
 # 主功能查询视图函数,处理查询用户的所有信息
 @app.route('/select2')
@@ -149,12 +156,12 @@ def select1(page=1):
     if request.method == 'POST':
         danhao = request.form.get('danhao')
         try:
-            if danhao and cdn_info_new.query.filter_by(url_dir=danhao).filter_by(user=session['user_username']).all():
-                pagination = cdn_info_new.query.filter_by(url_dir=danhao).filter_by(user=session['user_username']).order_by(cdn_info_new.create_time.desc()).paginate(page, per_page=15, error_out=False)
+            if danhao and cdn_info_new.query.filter_by(resources=danhao).filter_by(user=session['user_username']).all():
+                pagination = cdn_info_new.query.filter_by(resources=danhao).filter_by(user=session['user_username']).order_by(cdn_info_new.create_time.desc()).paginate(page, per_page=15, error_out=False)
                 data = pagination.items
-                return render_template('select2.html',data = data,user = session['user_username'],pagination=pagination)
+                return render_template('select2.html', data=data, user = session['user_username'], pagination=pagination)
             else:
-                return render_template('select2.html', data="none",user = session['user_username'],pagination="none")
+                return render_template('select2.html', data="none", user = session['user_username'], pagination="none")
         except Exception:
             print "DB Error."
             return None
@@ -165,7 +172,7 @@ def select1(page=1):
             if cdn_info_new.query.filter_by(user=session['user_username']).all():
                 pagination = cdn_info_new.query.filter_by(user=session['user_username']).order_by(cdn_info_new.create_time.desc()).paginate(page, per_page=15, error_out=False)
                 data = pagination.items
-                return render_template('select1.html',data = data,user = session['user_username'],pagination=pagination)
+                return render_template('select1.html', data=data, user=session['user_username'], pagination=pagination)
             else:
                 return render_template('select1.html', data="none", user=session['user_username'], pagination="none")
         except Exception:
@@ -173,19 +180,20 @@ def select1(page=1):
             return None
         finally:
             db.session.close()
-            
+
+
 # 主功能查询视图函数,处理用户查询某个url的信息
 @app.route('/select2/<int:page>/')
 @login_required
 def select2(page=1):
     danhao = request.args.get('danhao')
     try:
-        if danhao and cdn_info_new.query.filter_by(url_dir=danhao).filter_by(user=session['user_username']).all():
-            pagination = cdn_info_new.query.filter_by(url_dir=danhao).filter_by(user=session['user_username']).order_by(cdn_info_new.create_time.desc()).paginate(page, per_page=15, error_out=False)
+        if danhao and cdn_info_new.query.filter_by(resources=danhao).filter_by(user=session['user_username']).all():
+            pagination = cdn_info_new.query.filter_by(resources=danhao).filter_by(user=session['user_username']).order_by(cdn_info_new.create_time.desc()).paginate(page, per_page=15, error_out=False)
             data = pagination.items
-            return render_template('select2.html',data = data,user = session['user_username'],pagination=pagination)
+            return render_template('select2.html', data=data, user=session['user_username'], pagination=pagination)
         else:
-            return render_template('select2.html', data="none",user = session['user_username'],pagination="none")
+            return render_template('select2.html', data="none", user=session['user_username'], pagination="none")
     except Exception:
         print "DB Error."
         return None
@@ -226,4 +234,8 @@ def flush_resource():
     return jsonify({'resource': item_id}), 201
 
 if __name__ == '__main__':
+    # from werkzeug.contrib.fixers import ProxyFix
+    # app.wsgi_app = ProxyFix(app.wsgi_app)
+    # gunicorn -w 4 -b 127.0.0.1:5000 CDN_Refresh:app       //use gunicorn to run the app.
     app.run(host="0.0.0.0", port=5000)
+    
