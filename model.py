d1a0e35bfec523ec61701942d614cea39a98e1ba
wsjhk <2621570425@qq.com>
2018-03-10 14:51:49 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 # -*- coding: utf8 -*-
+# 说明：flask数据模型
 
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
@@ -33,19 +34,36 @@ class auth_user(db.Model):
         self.email = email
         self.password = password
 
+# class cdn_info(db.Model):
+#     __tablename__ = 'cdn_info'
+#     id = db.Column(db.Integer,primary_key=True,autoincrement=True)
+#     RefreshTaskId = db.Column(db.Integer,nullable=False)
+#     url_dir = db.Column(db.String(255),nullable=False)
+#     creat_time = db.Column(db.DateTime,default=datetime.now)
+#     Process = db.Column(db.String(10),nullable=False)
+#     user = db.Column(db.String(50),nullable=False)
+#
+#     def __init__(self,RefreshTaskId,url_dir,Process,user):
+#         self.RefreshTaskId = RefreshTaskId
+#         self.url_dir = url_dir
+#         self.Process = Process
+#         self.user = user
+
 class cdn_info_new(db.Model):
     __tablename__ = 'cdn_info_new'
     id = db.Column(db.Integer,primary_key=True,autoincrement=True)
     RefreshTaskId = db.Column(db.Integer,nullable=False)
-    url_dir = db.Column(db.String(255),nullable=False)
+    resources = db.Column(db.String(255),nullable=False)
+    type = db.Column(db.Enum("url","dir"), default="url")
     create_time = db.Column(db.DateTime,default=datetime.now,nullable=False)
     Process = db.Column(db.String(10),nullable=False)
     user = db.Column(db.String(50),nullable=False)
     status = db.Column(db.Enum("1","2","3","4"), default="1")   #表示刷新的状态，1表示等待刷新，2表示刷新中，3表示已刷新，4表示刷新失败
 
-    def __init__(self,RefreshTaskId,url_dir,Process,user,status):
+    def __init__(self,RefreshTaskId,resources,type,Process,user,status):
         self.RefreshTaskId = RefreshTaskId
-        self.url_dir = url_dir
+        self.resources = resources
+        self.type = type
         self.Process = Process
         self.user = user
         self.status = status
